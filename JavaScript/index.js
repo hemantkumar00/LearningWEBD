0c6670999176d77558c43dbbff89c8244d70ec7c
hemantkumar00 <hemantkumar23090@gmail.com>
2023-04-29 15:40:01 +0530

    Learning GIT and WEB Development

---
+++ JavaScript/index.js
@@ -0,0 +1,40 @@
+// const arr = [2, 3, 4, 5, 6, 7, 8, 9];
+
+// const result = arr.find((value, index) => {
+//   if (value >= 8) return value;
+// });
+
+// const result = arr.filter((value, index) => {
+//   if (value > 4) return value;
+// });
+
+// const result = arr.every((value, index) => {
+//   return value >= 2;
+// });
+
+// const result = arr.some((value, index) => {
+//   return value > 2;
+// });
+
+// console.log(result);
+
+// for (let element of arr) {
+//   element += 4;
+//   console.log(element);
+// }
+// console.log(arr);
+
+// arr.forEach((value, index) => {
+//   console.log(value);
+// });
+
+// const ans = arr.map((value, index) => {
+//   return value + 20;
+// });
+// console.log(ans);
+
+// const result = arr.reduce((previousValue, currentValue, index) => {
+//   return previousValue + currentValue;
+// });
+
+// console.log(result);
