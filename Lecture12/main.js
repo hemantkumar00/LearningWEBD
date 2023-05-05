59671663bc11ca4186f99ddf4d2f0ecd448c55b3
hemantkumar00 <hemantkumar23090@gmail.com>
2023-05-05 23:03:34 +0530

    Started JS

---
+++ Lecture12/main.js
@@ -0,0 +1,15 @@
+// console.log("hello world");
+
+// Variables in Java-Script
+
+a = 10; // By default its Var
+
+let message;
+
+// console.log(message);
+
+message = "Hemant";
+
+console.log(message);
+
+// console.log(a);
