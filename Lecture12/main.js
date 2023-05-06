b1713e9eba023e6fcd04015b76b0a5a0bbc45d0b
hemantkumar00 <hemantkumar23090@gmail.com>
2023-05-06 12:10:19 +0530

    Scope

--- Lecture12/main.js
+++ Lecture12/main.js
@@ -13,3 +13,31 @@ message = "Hemant";
 console.log(message);
 
 // console.log(a);
+
+// Function in JS
+
+// Function Decleration
+
+function print() {
+  console.log("Hello world");
+}
+
+// print();
+
+// Function Expression
+
+let twoNo = function () {
+  let a = 10;
+  let b = 12;
+  return a + b;
+};
+
+console.log(twoNo());
+
+// Arrow function
+
+let fun = () => {
+  console.log("Kuch Bhi");
+};
+
+fun();
