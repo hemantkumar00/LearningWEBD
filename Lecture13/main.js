d40f1d55022b5c3564666d8535dd07410ba8e44d
hemantkumar00 <hemantkumar23090@gmail.com>
2023-05-06 17:00:39 +0530

    Basic JS

---
+++ Lecture13/main.js
@@ -0,0 +1,25 @@
+// alert("Hello World");
+
+// let age = prompt("Enter you age", 18);
+
+// let confirmAge = confirm("Are yo 30 years ");
+
+function typeConversion() {
+  let inputElement = document.getElementById("inputelement").value;
+  console.log(inputElement);
+}
+
+//Loops
+
+// let i = 3;
+// while (i) {
+//   alert(i);
+//   i--;
+// }
+
+for (let j = 0; j < 10; j++) {
+  console.log(j);
+}
+
+console.log(parseFloat("13231.23423px"));
+//Opertors
