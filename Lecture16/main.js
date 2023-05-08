95a672cdf269998dfc1e4110bce7ffaae69359fa
hemantkumar00 <hemantkumar23090@gmail.com>
2023-05-09 00:28:28 +0530

    DOM Event

--- Lecture16/main.js
+++ Lecture16/main.js
@@ -1,52 +1,52 @@
-// console.dir(document);
+console.dir(document);
 
-// console.log(document.URL);
+console.log(document.URL);
 
-// console.log(document.title);
+console.log(document.title);
 
-// document.title = "hello";
+document.title = "hello";
 
-// console.log(document.body.childNodes);
+console.log(document.body.childNodes);
 
-// console.log(document.body.children);
+console.log(document.body.children);
 
-// console.log(document.all[11].children[0].textContent);
+console.log(document.all[11].children[0].textContent);
 
-// document.all[11].children[0].textContent = "Kam kar aapna ";
+document.all[11].children[0].textContent = "Kam kar aapna ";
 
-// let mainHeader = document.getElementById("header-title");
-// console.log(mainHeader.textContent);
+let mainHeader = document.getElementById("header-title");
+console.log(mainHeader.textContent);
 
-// mainHeader.textContent = "This is new List";
+mainHeader.textContent = "This is new List";
 
-// mainHeader.innerHTML = "<p>This is a nested paragraph</p>";
+mainHeader.innerHTML = "<p>This is a nested paragraph</p>";
 
-// console.log((mainHeader.style.color = "red"));
+console.log((mainHeader.style.color = "red"));
 
-// Object.assign(mainHeader.style, { backgroundColor: "aqua", color: "blue" });
+Object.assign(mainHeader.style, { backgroundColor: "aqua", color: "blue" });
 
-// let liContent = document.getElementsByClassName("list-group-item");
+let liContent = document.getElementsByClassName("list-group-item");
 
-// console.log(liContent);
+console.log(liContent);
 
-// for (let element = 0; element < liContent.length; element++) {
-//   console.log((liContent[element].style.color = "blue"));
-// }
+for (let element = 0; element < liContent.length; element++) {
+  console.log((liContent[element].style.color = "blue"));
+}
 
-// let inputElement = document.getElementsByTagName("input");
-// console.log(inputElement);
+let inputElement = document.getElementsByTagName("input");
+console.log(inputElement);
 
 // let inputElement = document.querySelector("")
 
-// let ulElement = document.querySelector("#items");
+let ulElement = document.querySelector("#items");
 
-// console.log(
-//   ulElement.firstElementChild.nextElementSibling.nextElementSibling.textContent,
-// );
+console.log(
+  ulElement.firstElementChild.nextElementSibling.nextElementSibling.textContent,
+);
 
-// let firstElement = document.querySelector("#main");
+let firstElement = document.querySelector("#main");
 
-// console.log(firstElement.parentElement);
+console.log(firstElement.parentElement);
 
 // Create Element
 
@@ -66,3 +66,31 @@ console.log(appendParentElement);
 let headerElement = document.querySelectorAll(".container");
 
 headerElement[1].insertBefore(newDivContainer, appendParentElement);
+
+function clickHandler(event) {
+  console.log("hello world");
+}
+
+// Event Listner
+
+let btn = document.querySelector("button");
+// console.log(btn);
+
+btn.addEventListener("click", (event) => {
+  console.log(event.target);
+  btn.className = "btn";
+  btn.textContent = "submit button";
+});
+
+let formElement = document.querySelector("form");
+
+formElement.addEventListener("submit", (event) => {
+  event.preventDefault();
+  console.log(event.target[0].value);
+  let newLi = document.createElement("li");
+  newLi.className = "list-group-item";
+  newLi.appendChild(document.createTextNode(event.target[0].value));
+
+  let ulElement = document.querySelector("#items");
+  ulElement.appendChild(newLi);
+});
