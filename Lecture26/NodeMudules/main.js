d606120f2b2bf411213d4813689cc4edb249cb78
hemantkumar00 <hemantkumar23090@gmail.com>
2023-05-20 13:59:30 +0530

    Started learning Node

---
+++ Lecture26/NodeMudules/main.js
@@ -0,0 +1,48 @@
+const http = require("http");
+
+// const os = require("os");
+
+// console.log(os);
+// const user = os.userInfo();
+// console.log(user);
+
+// console.log(os.uptime());
+
+// console.log(os.platform());
+
+// console.log(os.freemem());
+
+// console.log(os.cpus());
+
+// console.log(os.totalmem());
+
+//TODO:
+//Path
+
+// const path = require("path");
+
+// console.log(path.sep);
+
+// let filePath = path.join("./main", "./content", "abc.txt");
+// filePath = path.resolve("../", "./main", "./content", "abc.txt"); // absolute path
+
+// console.log(filePath);
+
+// filePath = path.join(__dirname, "main.js"); // relative path
+// //both works similar
+// console.log(filePath);
+
+//TODO: HTTP
+
+// const server = http.createServer((req, res) => {
+//   console.log("server started");
+//   if (req.url === "/") {
+//     res.write("welcome to http module");
+//     res.end();
+//   } else if (req.url === "/about") {
+//     res.write("welcome to about");
+//     res.end();
+//   }
+// });
+
+// server.listen(8080);
